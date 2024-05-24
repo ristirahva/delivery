65d69fa98402b00cd723320ecb2dc0886c0fc53a
Konstantin Menshikov <konstantin.menshikoff@gmail.com>
2024-05-24 10:07:33 +0300

    Задание 8 (промежуточная нерабочая версия)

---
+++ src/main/java/ru/task/deliveryapp/api/adapters/kafka/basketconsumed/Consumer.java
@@ -0,0 +1,16 @@
+package ru.task.deliveryapp.api.adapters.kafka.basketconsumed;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import ru.task.deliveryapp.ContractKafka;
+
+@Service
+public class Consumer {
+    private static final Logger log = LoggerFactory.getLogger(Consumer.class);
+    @KafkaListener(topics = "basket.confirmed", groupId = "NotificationConsumerGroup")
+    public void listenBasket(ContractKafka.BasketConfirmedIntegrationEvent message) {
+        log.info("!!!!! Received Message in topic basket.confirmed: " + message.getBasketId());
+    }
+}
---
+++ src/main/java/ru/task/deliveryapp/configuration/KafkaConfig.java
@@ -0,0 +1,49 @@
+package ru.task.deliveryapp.configuration;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.UUIDDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import ru.task.deliveryapp.ContractKafka;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+public class KafkaConfig {
+    @Bean
+    KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<Integer, String>>
+    kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<Integer, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        factory.setConcurrency(3);
+        factory.getContainerProperties().setPollTimeout(3000);
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<Integer, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
+    }
+
+    @Bean
+    public Map<String, Object> consumerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, UUIDDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, ContractKafka.BasketConfirmedIntegrationEvent.class);
+
+        return props;
+    }
+}
